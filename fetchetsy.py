1027c1d76ba81b9022dbe65ee1cd2a9c4c6878eb
Yusuf Eren <yusuf.eren.2014@hotmail.com>
2022-05-19 22:29:38 +0300

    final_version

--- fetchetsy.py
+++ fetchetsy.py
@@ -9,9 +9,9 @@ list_sku1 = list(df1['SKU'])
 list_image1 = list(df1['IMAGE1'])
 
 # We are going to fetch all links in list_image1
-for i in list_image1:
-    r = requests.get(i).content
+for i in range(0,len(list_image1)):
+    r = requests.get(list_image1[i]).content
     # Naming photos with their SKUs
-    for x in range(0, len(list_sku1)):
-        with open((str(list_sku1[x]) + ".jpg"),"wb+") as f:
-            f.write(r)
+    with open((str(list_sku1[i]) + ".jpg"),"wb+") as f:
+        f.write(r)
+        
